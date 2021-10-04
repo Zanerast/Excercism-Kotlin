5d88e1ec0715b00834f007bc9bda2db06cbd8d1e
Zane Rastrick <zanerast@gmail.com>
2021-10-05 07:14:52 +1300

    Finish Diamond Printer excercise

---
+++ src/main/kotlin/DiamondPrinter.kt
@@ -0,0 +1,20 @@
+class DiamondPrinter {
+
+    fun printToList(letter: Char): List<String> {
+
+        require(letter.toString() in "A".."Z")
+
+        // Create list that will be transformed
+        // eg C will create A,B,C,B,A
+        val list: MutableList<Char> = mutableListOf()
+        list.also {
+            it.addAll('A'..letter)
+            it.addAll((letter downTo 'A').drop(1))
+        }
+
+        return list.map {
+            val left = " ".repeat(letter - it).plus(it).plus(" ".repeat(it - 'A'))
+            left.plus(left.reversed().drop(1))
+        }
+    }
+}
